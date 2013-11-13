14e7912b44f22869c18adfae1fddfa464e6f1825
Jamie Hodge <jamiehodge@me.com>
2013-11-13 16:27:39 +0100

    add null_dataset plugin

--- lib/media/persistence/db.rb
+++ lib/media/persistence/db.rb
@@ -3,7 +3,7 @@ require "sequel"
 module Media
   module Persistence
     DB = Sequel.connect(ENV["DATABASE_URL"]).tap do |db|
-      db.extension :pg_streaming
+      db.extension :null_dataset, :pg_streaming
       db.stream_all_queries = true
     end
   end
