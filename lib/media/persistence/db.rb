6d804c1f9537cb7ada93d29a215b8b050a306f89
Jamie Hodge <jamiehodge@me.com>
2013-11-16 23:45:54 +0100

    add logger

--- lib/media/persistence/db.rb
+++ lib/media/persistence/db.rb
@@ -1,9 +1,11 @@
+require "logger"
 require "sequel"
 
 module Media
   module Persistence
     DB = Sequel.connect(ENV["DATABASE_URL"]).tap do |db|
       db.extension :null_dataset, :pg_streaming
+      db.loggers << Logger if ENV["DEBUG"]
       db.stream_all_queries = true
     end
   end
