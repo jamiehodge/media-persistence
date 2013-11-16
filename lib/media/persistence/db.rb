bedccf14f64ae98610e34373781390b4744cb468
Jamie Hodge <jamiehodge@me.com>
2013-11-16 23:46:44 +0100

    add pg_array

--- lib/media/persistence/db.rb
+++ lib/media/persistence/db.rb
@@ -3,8 +3,10 @@ require "sequel"
 
 module Media
   module Persistence
+    Sequel.extension :pg_array_ops
+
     DB = Sequel.connect(ENV["DATABASE_URL"]).tap do |db|
-      db.extension :null_dataset, :pg_streaming
+      db.extension :null_dataset, :pg_array, :pg_streaming
       db.loggers << Logger if ENV["DEBUG"]
       db.stream_all_queries = true
     end
